e3aac0a09c6649f247cad7b78870f719395d721c
gongxinya <63331601+gongxinya@users.noreply.github.com>
2023-01-26 18:21:58 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,42 @@
+input.onPinReleased(TouchPin.P2, function () {
+    serial.writeLine("h:5")
+})
+input.onPinReleased(TouchPin.P0, function () {
+    serial.writeLine("h:1")
+})
+input.onButtonPressed(Button.A, function () {
+    a = a + 1
+    if (a % 2 == 1) {
+        serial.writeLine("a:1")
+    } else {
+        serial.writeLine("a:0")
+    }
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    serial.writeLine("l:1")
+    serial.writeLine("r:0")
+})
+input.onButtonPressed(Button.B, function () {
+    b = b + 1
+    if (b % 2 == 1) {
+        serial.writeLine("b:1")
+    } else {
+        serial.writeLine("b:0")
+    }
+})
+input.onGesture(Gesture.TiltRight, function () {
+    serial.writeLine("r:1")
+    serial.writeLine("l:0")
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    serial.writeLine("t:-1")
+})
+input.onPinReleased(TouchPin.P1, function () {
+    serial.writeLine("h:3")
+})
+let a = 0
+let b = 0
+basic.forever(function () {
+    b = input.acceleration(Dimension.Z)
+    serial.writeLine("" + (b.toString()))
+})
